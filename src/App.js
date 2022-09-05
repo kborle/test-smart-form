59a2f882cbd980a52b41644caca348e00eceb88a
Kartikey Borle <kborle@brighttalk.com>
2022-09-05 17:38:36 +0530

    fix(): first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Button label="Submit button"/>
+    </div>
+  );
+}
+
+export default App;
